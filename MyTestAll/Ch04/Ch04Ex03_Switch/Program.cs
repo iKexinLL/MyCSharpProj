b489ea1aca7f15f96515ce4cc4a0f6716cf7f2b5
iKexinLL <kongpahuixiao@sina.com>
2017-08-30 22:40:28 +0800

    first commit

---
+++ MyTestAll/Ch04/Ch04Ex03_Switch/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch04Ex03_Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("box sizes:1=small, 5=medium, 10=large");
+            Console.WriteLine("请选择");
+            string s = Console.ReadLine();
+            //string s;
+            //s = Console.ReadLine();
+            int i = int.Parse(s);
+            int ct = 0;
+            switch (i)
+            {
+                case 1:
+                    ct += 1;
+                    break;
+                case 5:
+                    ct += 5;
+                    goto case 1;     //重要,可以使用goto语句进行控制,输入5的话,结果为6
+                case 10:
+                    ct += 10;
+                    goto case 1;
+
+                case 50:
+                case 100:
+                case 150: // 或者使用两个case直接相连,否则,每个case后面必须终止, break, goto, return(如需返回值的话)
+                default:
+                    Console.WriteLine("无效的输入。请选择1,5，or 10");
+                    break;
+            }
+            Console.WriteLine("谢谢！您的花费 = {0}", ct);
+            
+            while (i < 10)
+            {
+                Console.WriteLine($"{i++} year{(i == 1 ? "" : "s")}");
+            }
+
+            bool numOK = false;
+
+            int var1 = 0; // 代码1
+            int var2 = 0;
+            //int var1; // 代码2
+            //int var2;
+
+            while (!numOK) // 
+            {
+                Console.WriteLine("Enter var1");
+                var1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter var2");
+                var2 = Convert.ToInt32(Console.ReadLine());
+
+                if ((var1 > 10) || (var2 > 10))
+                    Console.WriteLine("Only one number can be greater than 10");
+                else
+                    numOK = true;
+
+                //Console.WriteLine($"you enterd {var1} and {var2}."); //或者写在这里
+            }
+
+            //如果没有初始化的话(调用代码2), 放在这里会报错
+            //因为在while判断中 !numOK是无法访问的,导致这里的var1和var2没有初始化,导致报错
+            //如果while条件为 1==1,则没有问题.因为下面总会访问到
+            //所以尽量初始化
+            Console.WriteLine($"you enterd {var1} and {var2}.");
+            while (1 == 1)
+            {
+                var1 = Convert.ToInt32(Console.ReadLine());
+                var2 = Convert.ToInt32(Console.ReadLine());
+                break;
+            }
+            Console.WriteLine($"you enterd {var1} and {var2}.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MyTestAll/Ch05/Ch05/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch05
+{
+    // 类似一个类.没有访问标识
+    enum Orientation : byte // -> 指定类型
+    {
+        north = 1, // 如果不给定值,则默认为0,然后依次加一
+        south = 2, // 允许多个枚举成员有相同的值。没有显示赋值的枚举成员的值，总是前一个枚举成员的值+1
+        east, // 其实是3
+        west = 4
+    }
+
+    enum Days { Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday, Friday };
+    enum BoilingPoints { Celsius = 100, Fahrenheit = 212 };
+    [FlagsAttribute]
+    enum Colors { Red = 1, Green = 2, Blue = 4, Yellow = 8 };
+
+    //定义一个结构,注意结构里面的变量是有可访问性的.
+    struct Route
+    {
+        public Orientation direction;
+        public double distance;
+    }
+
+    class Program
+    {
+        // 不可以在方法里面定义
+        enum BoilingPoints { Celsius = 100, Fahrenheit = 212 };
+
+        static void Main(string[] args)
+        {
+            byte b;
+            short st = 233;
+            b = checked((byte)st); // check 检查溢出, 大于255会报错
+            Console.WriteLine(b);
+
+            byte directionByte;
+            string directionString;
+
+            Orientation myDirection = Orientation.north;
+            Console.WriteLine(myDirection);
+
+            directionByte = (byte)myDirection;
+            Console.WriteLine($"byte equivalent = {directionByte}");
+            // 无法使用(string)转换
+            // 等于 .ToString()
+            directionString = Convert.ToString(myDirection);
+            Console.WriteLine($"string equivalent = {directionString}");
+
+            string myString = "north";
+            //把string转化为枚举值
+            //(enumerationType)Enum.Parse(typeof(enumerationType), enumerationValueString);
+            Orientation myDirection2 = (Orientation)Enum.Parse(typeof(Orientation), myString);
+            Console.WriteLine($"string to enumeration, string is {myString}, enumeration is {myDirection2}");
+            Type typeOrientation = typeof(Orientation);
+            Console.WriteLine("start foreach Orientation, type is d");
+            foreach( string s in Enum.GetNames(typeOrientation))
+                Console.WriteLine(Enum.Format(typeOrientation, Enum.Parse(typeOrientation, s), "d"));
+            Console.WriteLine("start foreach Orientation, type is f");
+            foreach (string s in Enum.GetNames(typeOrientation))
+                Console.WriteLine(Enum.Format(typeOrientation, Enum.Parse(typeOrientation, s), "f"));
+            // 强转方式
+            Console.WriteLine("强转方式");
+            byte tst = (byte)Enum.Parse(typeof(Orientation), myString);
+            Console.WriteLine(tst);
+
+            //以下为测试
+            myDirection = (Orientation)st; 
+            Console.WriteLine(myDirection); // --> 直接显示233
+            st = 3;
+            myDirection = (Orientation)st;
+            Console.WriteLine(myDirection); // --> 显示为east
+
+            string codeHttp = "https://docs.microsoft.com/zh-cn/dotnet/api/System.Enum?view=netframework-4.7";
+            Console.WriteLine("以下为 {0} 上的代码", codeHttp);
+
+            Type weekdays = typeof(Days);
+            Type boiling = typeof(BoilingPoints);
+
+            Console.WriteLine("The days of the week, and their corresponding values in the Days Enum are:");
+
+            foreach (string s in Enum.GetNames(weekdays))
+                // Enum.Format说明: https://msdn.microsoft.com/zh-cn/library/system.enum.format(v=vs.110) --> 注意这个")",点击时要手动加上,否则访问不到
+                // 这篇文章也不错 http://www.cnblogs.com/claspcn/p/5218520.html
+                // 文章表现出可以当做"开关"控制
+                // enum（C# 参考） https://docs.microsoft.com/zh-cn/dotnet/csharp/language-reference/keywords/enum
+                Console.WriteLine("{0,-11}= {1}", s, Enum.Format(weekdays, Enum.Parse(weekdays, s), "d"));
+
+            Console.WriteLine();
+            Console.WriteLine("Enums can also be created which have values that represent some meaningful amount.");
+            Console.WriteLine("The BoilingPoints Enum defines the following items, and corresponding values:");
+
+            foreach (string s in Enum.GetNames(boiling))
+                Console.WriteLine("{0,-11}= {1}", s, Enum.Format(boiling, Enum.Parse(boiling, s), "d"));
+
+            Colors myColors = Colors.Red | Colors.Blue | Colors.Yellow;
+            Console.WriteLine();
+            Console.WriteLine("myColors holds a combination of colors. Namely: {0}", myColors);
+
+            Console.WriteLine("继续<入门经典>");
+            Route myRoute;
+            int newDirection = -1;
+            double myDistance;
+            Console.WriteLine("1) North\n2) South\n3) East\n4) West");
+            do
+            {
+                Console.WriteLine("Select a direction");
+                newDirection = Convert.ToInt32(Console.ReadLine());
+            }
+            while ((newDirection < 1) || (newDirection > 4));
+            Console.WriteLine("Input a distance");
+            myDistance = Convert.ToDouble(Console.ReadLine());
+            myRoute.direction = (Orientation)newDirection;
+            myRoute.distance = myDistance;
+            Console.WriteLine($"myRoute specifies a direction of {myRoute.direction} " +
+                $"and a distinct of {myRoute.distance}");
+
+            string dire = Enum.GetName(typeOrientation, newDirection);
+            myRoute.distance = myDistance;
+            Console.WriteLine($"myRoute specifies a direction of {dire} " +
+                $"and a distinct of {myRoute.distance}");
+
+            Console.ReadKey();
+        }
+    }
+}
