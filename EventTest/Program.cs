2379f08be752cab57c8e7058c3a3bbcc1bc56594
kongpahuixiao <kongpahuixiao@sina.com>
2019-01-23 17:00:16 +0800

    更新了关于事件的说明,更好的理解程序

--- EventTest/Program.cs
+++ EventTest/Program.cs
@@ -1,6 +1,23 @@
 ﻿using System;
 using System.Timers;
 
+/// <summary>
+/// 先定义一个委托MessageHandler
+/// 然后自定义一个这个此委托类型的事件MessageArrived
+/// 这样就有了一个自定义事件以及一个委托
+/// 定义CheckForMessage()方法,其中,会调用MessageArrived事件.
+/// 嗯...这个程序有两个事件,所以才会造成一些误解.
+/// myConnection.MessageArrived += new MessageHandler(myDisplay.DisplayMessage);
+/// MessageArrived订阅了myDisplay.DisplayMessage处理程序
+/// _pollTimer.Start();会调用_pollTimer.Elapsed中的处理程序CheckForMessage
+/// CheckForMessage会调用MessageArrived事件
+/// 然后MessageArrived才会调用所添加的处理程序myDisplay.DisplayMessage
+/// ---------------------
+/// _pollTimer.Elapsed += new ElapsedEventHandler(CheckForMessage);
+/// 这个命令在列表中添加一个处理程序,当引发Elapsed事件时,就会调用该处理程序.
+/// 可给这个列表添加任意多个处理程序,只要他们满足指定的条件.
+/// 当引发事件时,会依次调用每个处理程序.
+/// </summary>
 namespace EventTest
 {
     // Connection一个有事件的类
@@ -40,7 +57,7 @@ namespace EventTest
             Console.WriteLine("Checking for new messages");
             
             // ReSharper disable once UseNullPropagation
-            // 什么时候MessageArrived == null -> 手动设置
+            // 什么时候MessageArrived == null -> 暂且手动设置一下
             if (MessageArrived == null)
             {
                 System.Console.WriteLine("Event is null;");
@@ -62,6 +79,12 @@ namespace EventTest
             => Console.WriteLine($"Message arrived {message}");
     }
 
+    public class Display2
+    {
+        public void DisplayTestMessage(string testMessage)
+            => Console.WriteLine($"This is a Test Message {testMessage}");
+    }
+
     class Program
     {
         public static void TestMessage(string message)
@@ -72,10 +95,12 @@ namespace EventTest
             // 这两个类通过事件,委托连接了起来
             Connection myConnection = new Connection();
             Display myDisplay = new Display();
+            Display2 myDisplay2 = new Display2();
 
             // MessageArrived为一个事件,由委托MessageHandler指定了参数为string.
-            // 然后将
+            // 
             myConnection.MessageArrived += new MessageHandler(myDisplay.DisplayMessage);
+            myConnection.MessageArrived += new MessageHandler(myDisplay2.DisplayTestMessage);
             // myConnection.MessageArrived("TestMessage");
             // myConnection.MessageArrived += new MessageHandler(TestMessage);
             
