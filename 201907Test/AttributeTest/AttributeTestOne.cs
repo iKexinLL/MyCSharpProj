eafd1621d33b943e7f4c45437271761200fe9fa6
kongpahuixiao <kongpahuixiao@sina.com>
2019-07-04 19:04:45 +0800

    add MySelect method

--- 201907Test/AttributeTest/AttributeTestOne.cs
+++ 201907Test/AttributeTest/AttributeTestOne.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.InteropServices;
 
 namespace MyTest.AttributeTest
@@ -12,15 +14,19 @@ namespace MyTest.AttributeTest
             var myAttributes = Attribute.GetCustomAttributes(
                             typeof(AttributeTestOne), typeof(AttributeTestAttribute));
 
-            foreach (var myAttribute in myAttributes)
+            // MySelect(同Select)中,传入的(i,n)是对应的Func<TSource, int, TResult> selector中的TSource, int
+            // new {midAttribute = i, Index = n}的作用是将传入的(i, n)加工成字典
+            // 也就是说myAttribute的类型是....匿名类型.
+            // 原以为是字典的.
+            foreach (var myAttribute in myAttributes.MySelect((i, n) => new {midAttribute = i, Index = n}))
             {
                 // var mid = (AttributeTestAttribute)myAttribute;
-                Console.WriteLine($"myattribute.ClassName is {(myAttribute as AttributeTestAttribute).ClassName}, "
-                                + $"myattribute.Version is {(myAttribute as AttributeTestAttribute).Version})");
-                SetCursorPos(20, 20);
+                Console.WriteLine($"myattribute.ClassName is {(myAttribute.midAttribute as AttributeTestAttribute).ClassName}, "
+                                + $"myattribute.Version is {(myAttribute.midAttribute as AttributeTestAttribute).Version * myAttribute.Index})");
+                SetCursorPos(20 * myAttribute.Index, 20);
             }
             
-            MessageBox(0, "TestMessage", "My Message Box", 0);
+            MessageBox(0, "Remerber a \"Readkey\" at last line", "My Message Box", 0);
         }
 
         [DllImport("user32.dll")]
@@ -28,5 +34,21 @@ namespace MyTest.AttributeTest
 
         [DllImport("user32.dll")]
         private static extern bool SetCursorPos(int x, int y);  
+
+    }
+    
+    public static class TestThisSelect
+    {
+        public static IEnumerable<TResult> MySelect<TSource, TResult> (this IEnumerable<TSource> source, Func<TSource, int, TResult> selector)
+            // where TSource: new()
+        {
+            // 空值判断
+            int index = -1;
+            foreach (var i in source)
+            {
+                checked { ++index; }
+                yield return selector(i, index);
+            }
+        }
     }
 }
